247dbb370a27c9741df62221e976b9b01ed03445
Justin Geeslin <justin@geesl.in>
2018-05-08 16:37:44 -0500

    collection support

--- config.php
+++ config.php
@@ -3,5 +3,13 @@
 return [
     'baseUrl' => '',
     'production' => false,
-    'collections' => [],
+    'company' => 'Tighten Co',
+    'contact_email' => 'support@tighten.co',
+    'collections' => [
+        'posts' => [
+            'path' => 'blog/{date|Y-m-d}/{filename}',
+            'sort' => '-date',
+            'author' => 'Tighten Co.',
+        ],
+    ],
 ];
---
+++ source/sample-page.md
@@ -0,0 +1,13 @@
+---
+extends: _layouts.master
+title: My First Blog Post
+author: Keith Damiani
+date: 2017-03-23
+section: body
+---
+
+This post is *profoundly* interesting.
+
+# My awesome heading!
+
+My awesome content!
